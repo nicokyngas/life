3d9a16a8aa556a77ebd2eac1438d8a66ae6acbe4
nicokyngas <nico_kyngas@yahoo.co.uk>
2022-08-15 21:22:27 +0300

    Some more beautification

---
+++ src/definitions.h
@@ -0,0 +1,18 @@
+/**
+ * @file definitions.h
+ *
+ * @brief Aliases for types used often and in varying places
+ *
+ */
+
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+namespace life
+{
+
+    using board_dimension_type = unsigned short;
+
+}
+
+#endif // DEFINITIONS_H
